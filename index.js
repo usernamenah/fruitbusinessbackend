119e0370c03bac774cb10b956ed55a247db6b007
P.Sujith <sujithpappala777@gmail.com>
2025-04-02 10:43:58 +0530

    changed cors sets

--- index.js
+++ index.js
@@ -20,15 +20,21 @@ const client = new OAuth2Client(GOOGLE_CLIENT_ID);
 
 const getinfo = require('./getinfo.js');
 
-app.use(cors());
-
+const corsOptions = {
+    origin: 'https://fruitbusiness.vercel.app', // Only allow this origin
+    credentials: true, // Allow cookies, authentication headers
+    methods: 'GET, POST, PUT, DELETE, OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+  };
+  
+  app.use(cors(corsOptions));
 // CORS Configuration
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins (or specify a particular domain)
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins (or specify a particular domain)
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+//   });
 // app.use(
 //     cors({
 //         origin: "https://fruitbusiness.vercel.app",
