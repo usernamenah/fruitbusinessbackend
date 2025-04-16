0e39ede56a131d7b984794ff11cdf3eb25ad1f63
P.Sujith <sujithpappala777@gmail.com>
2025-04-16 10:12:56 +0530

    backend origin changed for test 1

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const placeorder = require('./booking.js');
 // CORS Configuration
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "https://fruitbusinessbackend.vercel.app",
         credentials: true,
         allowedHeaders: ["Content-Type", "Authorization"],
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
